03cfe0b52a2f3b379901a2afb3e25fbc609c99ec
networkSocerer <gigattfyhig@gmail.com>
2025-03-27 08:40:37 +0900

    정처기 C

---
+++ Project1/Project1/test7_4.cpp
@@ -0,0 +1,17 @@
+# include <stdio.h>
+struct insa {
+	char name[10];
+	int age;
+	struct insa* impl_a;
+	struct insa* impl_b;
+};
+
+int main() {
+	struct insa p1 = { "KIM",28,NULL,NULL };
+	struct insa p2 = { "LEE",36,NULL, NULL };
+	struct insa p3 = { "PARK",41,NULL,NULL };
+	p1.impl_a = &p2;
+	p2.impl_b = &p3;
+	printf("%s\n", p1.impl_a->name);
+	printf("%d", p2.impl_b->age);
+}
---
+++ Test6_3.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+main() {
+	char* p = "KOREA";
+	printf("1. %s\n", p);
+	printf("2. %s\n", p+1);
+	printf("3. %s\n", *p);
+	printf("4. %s\n", *(p+3);
+	printf("5. %s\n", *p+4);
+}
