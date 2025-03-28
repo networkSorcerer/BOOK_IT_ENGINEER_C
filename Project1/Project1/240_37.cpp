cb222751eae5d091d0b5b0bec86f011c4bba72e7
networkSocerer <gigattfyhig@gmail.com>
2025-03-28 17:10:43 +0900

    c 프로그래밍

---
+++ Project1/Project1/240_37.cpp
@@ -0,0 +1,17 @@
+//#include <stdio.h>
+//void prnt(int *a, int *b, int *c);
+//
+//int main() {
+//	int a = 0, b = 5, c = 0;
+//	prnt(&a, &b, &c);
+//	printf("a = %d, b = %d, c = %d\n", a, b, c);
+//}
+//
+//void prnt(int *x,int *y,int *z)
+//{
+//	while (*x < *y) {
+//		++*x;
+//		*z = *z + *x;
+//		prnt(x, y, z);
+//	}
+//}
