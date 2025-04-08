e2cc9b3494af975822577c4577b1304c4de9abe4
networkSocerer <gigattfyhig@gmail.com>
2025-04-08 17:25:17 +0900

    c

---
+++ Project1/Project1/109_15.cpp
@@ -0,0 +1,24 @@
+//#include <iostream>
+//
+//int len(const char* p);
+//
+//int main() {
+//    const char* p1 = "2022";
+//    const char* p2 = "202207";
+//
+//    int a = len(p1);
+//    int b = len(p2);
+//
+//    std::cout << a + b << std::endl;
+//
+//    return 0;
+//}
+//
+//int len(const char* p) {
+//    int r = 0;
+//    while (*p != '\0') {
+//        p++;
+//        r++;
+//    }
+//    return r;
+//}
---
+++ Project1/Project1/171_5.cpp
@@ -0,0 +1,10 @@
+//#include <stdio.h>
+//
+//int main() {
+//	const char* a = "qwer";
+//	const char* b = "qwtety";
+//	for (int i = 0; a[i] != '\n'; i++)
+//		for (int j = 0; b[j] != '\n'; j++)
+//			if (a[i] == b[j])
+//				printf("%c", a[i]);
+//}
