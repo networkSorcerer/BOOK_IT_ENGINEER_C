3635eeef1c1046354aae2aac285dc39f63bc6d40
networkSocerer <gigattfyhig@gmail.com>
2025-04-10 08:17:30 +0900

    c

---
+++ Project1/Project1/178_2.cpp
@@ -0,0 +1,16 @@
+//#include <stdio.h>
+//
+//int factorial(int n);
+//
+//int main() {
+//	int (*pf)(int);
+//	pf = factorial;
+//	printf("%d", pf(3));
+//}
+//
+//int factorial(int n) {
+//	if (n <= 1)
+//		return 1;
+//	else
+//		return n * factorial(n - 1);
+//}
